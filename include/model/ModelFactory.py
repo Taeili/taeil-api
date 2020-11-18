8a793508ad081f83b47fd05c69640425eae8b8d6
장태일 <taeil.jang@linkshops.com>
2020-11-18 23:15:38 +0900

    feat : model factory에 create, update, read, delete 추가

--- include/model/ModelFactory.py
+++ include/model/ModelFactory.py
@@ -5,3 +5,19 @@ class ModelFactory(object):
 	def get(self, Model, *args, **kwargs):
 		model = Model()
 		return model.get(*args, **kwargs)
+
+	def read(self, Model, *args, **kwargs):
+		model = Model()
+		return model.read(*args, **kwargs)
+
+	def create(self, Model, *args, **kwargs):
+		model = Model()
+		return model.create(*args, **kwargs)
+
+	def update(self, Model, *args, **kwargs):
+		model = Model()
+		return model.update(*args, **kwargs)
+
+	def delete(self, Model, *args, **kwargs):
+		model = Model()
+		return model.delete(*args, **kwargs)
