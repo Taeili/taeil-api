8941788aee142a2ceba5190b3785c7733d00c0d2
장태일 <taeil.jang@linkshops.com>
2020-11-19 23:36:56 +0900

    #7 : 웹 페이지 템플릿 적용

--- src/server/Server.py
+++ src/server/Server.py
@@ -6,7 +6,7 @@ from server.controller.user.User import user
 class Server(object):
 
 	def __init__(self):
-		self.app=Flask('API')
+		self.app=Flask('API', static_folder='templates/static')
 		self.app.register_blueprint(main)
 		self.app.register_blueprint(user)
 
